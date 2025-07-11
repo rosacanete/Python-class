a9e1567a0103c2ca90e028c9f1a42ba0b4a296a8
Rosa Canete <rosacanete06@gmail.com>
2025-07-11 16:54:35 -0300

    finalizdo operadores

---
+++ clase2/operadores.py
@@ -0,0 +1,23 @@
+''' 
+a == b
+a != b 
+a > b
+a < b
+a >= b
+a <= b
+#operadores de comparación
+'''
+a= 17
+b= 18
+r=a == b
+r= a != b
+print(r)
+r= a > b 
+print("es menor que", r)
+r= a < b
+print("es mayor que", r)
+r= a <= b
+print(r)
+r= a >= b
+print(r)
+
--- clase2/variables.py
+++ clase2/variables.py
@@ -1,11 +1,5 @@
-Voluptate in exercitation culpa aliqua officia nulla amet. In nulla excepteur Lorem sunt non do labore. Occaecat irure nostrud nostrud cillum proident sunt culpa adipisicing. Cillum ullamco magna aliquip velit veniam laborum magna non laborum labore pariatur ullamco aliquip aute.
 
-Irure velit dolore ullamco incididunt elit cillum ullamco consectetur deserunt. Culpa consectetur veniam excepteur dolore in. Ipsum aute deserunt nisi et sint et sit esse pariatur culpa cillum. Magna cupidatat laborum ullamco est occaecat et sint elit. Reprehenderit quis quis sit voluptate officia reprehenderit voluptate deserunt esse.
-
-Nostrud do reprehenderit veniam pariatur incididunt non commodo. Dolor exercitation aute nostrud ad exercitation id eiusmod. Veniam duis dolore proident ea non ullamco fugiat et duis adipisicing magna laboris aute Lorem. Ad in veniam sint aute exercitation ad elit cupidatat ipsum qui nisi consectetur reprehenderit. Sunt est esse anim enim mollit nostrud et id commodo.
-
-In amet ea occaecat tempor mollit nisi elit mollit ad adipisicing enim duis eu labore. Commodo et ullamco ea cillum incididunt enim amet. Fugiat incididunt eiusmod enim aliquip aliquip culpa magna incididunt nisi qui proident aliqua irure. Adipisicing dolor qui ex culpa est nisi consequat mollit excepteur. Ipsum officia aute sunt est ad occaecat non.
-print("Hola mundo desde la Unida ")esse
+print("Hola mundo desde la Unida ")
 entero= 10
 flotante= 10.5
 cadena= "Hola mundo"   
