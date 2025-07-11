5a78ec1dfcf302acebaf173b277e15d3a0b7d585
Rosa Canete <rosacanete06@gmail.com>
2025-07-11 15:38:59 -0300

    Finalizado Variables

---
+++ clase2/variables.py
@@ -0,0 +1,28 @@
+Voluptate in exercitation culpa aliqua officia nulla amet. In nulla excepteur Lorem sunt non do labore. Occaecat irure nostrud nostrud cillum proident sunt culpa adipisicing. Cillum ullamco magna aliquip velit veniam laborum magna non laborum labore pariatur ullamco aliquip aute.
+
+Irure velit dolore ullamco incididunt elit cillum ullamco consectetur deserunt. Culpa consectetur veniam excepteur dolore in. Ipsum aute deserunt nisi et sint et sit esse pariatur culpa cillum. Magna cupidatat laborum ullamco est occaecat et sint elit. Reprehenderit quis quis sit voluptate officia reprehenderit voluptate deserunt esse.
+
+Nostrud do reprehenderit veniam pariatur incididunt non commodo. Dolor exercitation aute nostrud ad exercitation id eiusmod. Veniam duis dolore proident ea non ullamco fugiat et duis adipisicing magna laboris aute Lorem. Ad in veniam sint aute exercitation ad elit cupidatat ipsum qui nisi consectetur reprehenderit. Sunt est esse anim enim mollit nostrud et id commodo.
+
+In amet ea occaecat tempor mollit nisi elit mollit ad adipisicing enim duis eu labore. Commodo et ullamco ea cillum incididunt enim amet. Fugiat incididunt eiusmod enim aliquip aliquip culpa magna incididunt nisi qui proident aliqua irure. Adipisicing dolor qui ex culpa est nisi consequat mollit excepteur. Ipsum officia aute sunt est ad occaecat non.
+print("Hola mundo desde la Unida ")esse
+entero= 10
+flotante= 10.5
+cadena= "Hola mundo"   
+booleano= True
+print(entero, flotante, cadena, booleano)
+print(type(entero), type(flotante), type(cadena), type(booleano))
+
+print("suma de dos números")
+#Operación Básica con Variables
+#Suma, Resta, Multiplicación y División
+a=10
+b=20
+print("suma de dos números:", a + b)
+print("resta de dos números")
+print("resta de dos números:", a - b)
+print("multiplicación de dos números")
+print("multiplicación de dos números:", a * b)   
+b= 2   
+print("división de dos números")
+print("división de dos números:", a / b)
