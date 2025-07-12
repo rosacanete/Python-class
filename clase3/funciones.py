34acdd356504ae2e593ab387573db2842ad0482e
Rosa Canete <rosacanete06@gmail.com>
2025-07-12 13:54:33 -0300

    funciones

---
+++ clase3/funciones.py
@@ -0,0 +1,20 @@
+#fnciones en C++
+#include <iostream>
+#using namespace std;
+#void sumar (int a, int b) Prototipo de funcion
+#int main() {
+#int a = 5;
+#int b = 10;
+#sumar(a, b);
+#cout << "" << sumar<< endl; // Llamada a la funcion
+#return 0;
+a=2
+b=3
+def sumar(a, b):
+    return a + b #Definicion de la funcion sumar
+
+print("Resultado de la suma:", sumar(a, b)) # Imrprimir el resultado de la suma
+def restar(a, b):
+    return a - b #Definicion de la funcion restar
+
+print("Resultado de la resta:", restar(a, b)) # Imprimir el resultado de la resta
