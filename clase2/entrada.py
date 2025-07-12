c014eefc42248599edf16b12a8bf1e5d35766127
Rosa Canete <rosacanete06@gmail.com>
2025-07-11 23:25:19 -0300

    entrada

---
+++ clase2/entrada.py
@@ -0,0 +1,7 @@
+'''
+Entrada de datos por teclado
+'''
+cadena = input("Introduce nombre de su proyecto: ")
+print(f"El nombre de su proyecto es: {cadena}")
+numero = int(input("¿ Que version es?: "))
+print(f"La version de su proyecto es: {numero+1}")
