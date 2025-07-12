742b38aa941d0fbe42cafca4b53a0c91bbb917f4
Rosa Canete <rosacanete06@gmail.com>
2025-07-11 23:41:54 -0300

    condicionales

---
+++ clase2/condicionales-if.py
@@ -0,0 +1,10 @@
+'''
+condicionales if
+'''
+dato=input("Introduce un numero: ")
+if dato > 0: 
+    print("El numero es positivo")  
+elif dato < 0:
+    print("El numero es negativo")
+else:
+    print("El numero es cero")  
