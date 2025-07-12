67a7a2af0ebbee905e404609e122005d35a92064
Rosa Canete <rosacanete06@gmail.com>
2025-07-11 22:23:09 -0300

    operadores aritmeticos

---
+++ clase2/aritmeticos.py
@@ -0,0 +1,28 @@
+'''
+operaciones aritmeticas básicas en Python
+suma, resta, multiplicación y división dos números
+c=a + b
+d=a - b
+e=a * b
+f=a / b
+h= a // b
+i= a % b
+j= a ** b
+'''
+a= 17
+b= 18
+c = a + b
+d = a - b 
+e = a * b
+f = a / b
+h = a // b
+i = a % b
+j = a ** b
+print("suma de dos números:", c)
+print("resta de dos números:", d)
+print("multiplicación de dos números:", e)
+print("división de dos números:", f)
+print("división entera de dos números:", h)
+print("módulo de dos números:", i)
+print("potencia de dos números:", j)
+print("fin de las operaciones aritméticas")
